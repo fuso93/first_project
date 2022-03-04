a9937eb9f46e901f0a5712fbd842e48ec2c6331f
fuso <fuso93@naver.com>
2022-03-04 22:13:40 +0900

    add TabNavigate #2

--- App.js
+++ App.js
@@ -6,14 +6,41 @@
 import React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Text } from "react-native";
 import { Home, Profile, Alarm, Chat } from './screens';
-
+import {Ionicons} from "@expo/vector-icons";
 const  Tab = createBottomTabNavigator();
 
 const App = () => {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+                        if (route.name === "Home") {
+                            iconName = focused
+                                ? ("ios-home")
+                                : ("home-outline")
+                        } else if ( route.name === "Chat") {
+                            iconName = focused
+                                ? ("chatbox-ellipses")
+                                : ("chatbox-ellipses-outline")
+                        } else if ( route.name === "Alarm"){
+                            iconName = focused
+                                ? ("alarm-sharp")
+                                : ("alarm-outline")
+                        } else {
+                            iconName = focused
+                                ? ("person-circle")
+                                : ("person-circle-outline")
+                        }
+                        return <Ionicons name={iconName} size={size} color={color} />
+                    },
+                    tabBarActiveTintColor: 'tomato',
+                    tabBarInactiveTintColor: 'lightGray'
+                })}
+            >
                 <Tab.Screen name={"Home"} component={Home} />
                 <Tab.Screen name={"Chat"} component={Chat} />
                 <Tab.Screen name={"Alarm"} component={Alarm}/>
